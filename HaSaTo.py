1211605fcca1f7831bc74e73185b6439fa5d7e10
hvt206 <113999132+hvt206@users.noreply.github.com>
2022-10-14 12:06:31 +0700

    HaSaTo.py

---
+++ HaSaTo.py
@@ -0,0 +1,57 @@
+from multiprocessing.connection import wait
+import socket
+import os
+import random
+import getpass
+import time
+import sys
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+proxys = open('proxies.txt').readlines()
+bots = len(proxys)
+methods = open('methods.gif').readlines()
+bots = len(methods)
+
+
+def si():
+    print('\x1b[38;2;233;233;233mWelcome to HaSaTo DDos ')
+    print("")
+
+def special():
+    clear()
+    si()
+    print("             Siiiiiiiii")
+
+def menu():
+    clear()
+    print("               Ra Menu R lòn")
+
+
+
+os.system('cls' if os.name == 'nt' else 'clear')
+
+
+
+
+def methods():
+    clear()
+    si()
+    print("             Mê Thon Balabalalalalal")
+            
+def login():
+    clear()
+    user = "admin"
+    passwd = "thanhtrungancut"
+    username = input("⚡ Username: ")
+    password = getpass.getpass(prompt='⚡ Password: ')
+    if username != user or password != passwd:
+        print("")
+        print("⚡ Sai rồi nhé ib ChungTrong để cấp lại tool...")
+        sys.exit(1)
+    elif username == user and password == passwd:
+        print("⚡ Welcome to ChungTrong Panel!")
+        time.sleep(0.3)
+        main()
+login()
